3b88ef24ac8b01eed2eebc93647896c0e7f5fe70
ChristopherPerrault <christopherperrault@hotmail.com>
2022-12-03 14:15:50 -0500

    imported Outlet

--- frontend/src/layouts/MainLayout.js
+++ frontend/src/layouts/MainLayout.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
-import Outlet from "../components/Outlet";
+import { Outlet } from "react-router-dom";
 
 export default function MainLayout() {
-    return (
-        <div>
-            <Header />
-            <Outlet />
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  );
 }
