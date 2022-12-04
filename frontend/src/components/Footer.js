2109e1213d6c970e8350aed2b4fc4afbee3b8b0e
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2022-12-04 09:10:08 -0500

    added a very basic footer to the layout

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import "./Footer.css";
+
+function Footer() {
+  
+
+  return (
+    <div className="footer">
+      Copyright &copy; 2022 The REGEX
+    </div>
+  );
+}
+
+export default Footer;
--- frontend/src/layouts/MainLayout.js
+++ frontend/src/layouts/MainLayout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 import { Outlet } from "react-router-dom";
 
 export default function MainLayout() {
@@ -7,6 +8,7 @@ export default function MainLayout() {
     <div>
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 }
