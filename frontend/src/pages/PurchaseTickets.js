45c5b0e4d1730e684d4234058318ea2f17fd9c51
Kevin Darby <92401368+kevindarbydev@users.noreply.github.com>
2022-12-07 07:15:10 -0500

    Update PurchaseTickets.js

--- frontend/src/pages/PurchaseTickets.js
+++ frontend/src/pages/PurchaseTickets.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import ProductCard from "../components/ProductCard";
 import "../index.css";
 import {
   Card,
@@ -15,44 +14,107 @@ import {
 export default function PurchaseTicket(props) {
   document.title = "TicketBlaster | Event Details";
 
-  const productId = "price_1MC3E3DM4nrUdWXdpVAePbuO";
+  const [events, setEvents] = useState([]);
   const params = useParams();
-//"https://app.ticketmaster.com/discovery/v2/events/" + id + ".json?apikey=zhJDIqfMsloXKpRjywIbmnUSBGw9AxNq";
- 
-        const id = params.id.toString();
-    displayData(id);
- 
+
+  useEffect(() => {
+    const url =
+      // Chris:
+      // "https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=522&apikey=zhJDIqfMsloXKpRjywIbmnUSBGw9AxNq";
+
+      // Matt #1:
+      //"https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=522&apikey=qiJ5AkBhkvr6IFdd9UamBev1hYovx46M";
+
+      // Matt #2:
+      // https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=522&apikey=hu5LlW3eJkDoVLKyUGnAZmbpZS8k6eCE111
+
+      // Kevin:
+      "https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=522&apikey=LVpUpS7gaeXxvoQMKgDF1zSNbXbASUgS";
+
+    const loadData = async () => {
+      const id = params.id.toString();
+      await fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          setEvents(
+            data._embedded.events.map((event, index) => {
+              if (event.id === id) {
+                const date = new Date(event.dates.start.localDate);
+                const formattedDate = date.toLocaleDateString("en-CA", {
+                  weekday: "long",
+                  day: "numeric",
+                  month: "long",
+                  year: "numeric",
+                });
+
+                return (
+                  <div>
+                    <Box>
+                      <Card className="indv-event-details-container">
+                        <CardActions>
+                          <CardMedia
+                            height="400"
+                            width="400"
+                            component="img"
+                            srcSet={event.images[6].url}
+                            title={event.name + " banner image"}
+                            alt={event.name + " banner image"}
+                          />
+                        </CardActions>
+                        <CardMedia />
+                        <CardContent>
+                          <Typography variant="h4" component="h4">
+                            {event.name}
+                          </Typography>
+                          <Typography variant="body2" component="h4">
+                            {formattedDate}
+                            <br />
+                            <br />
+                            Performing LIVE at: <br />
+                            {event._embedded.venues[0].name}
+                            <br />
+                            {event._embedded.venues[0].city.name},{"\u00A0"}
+                            {event._embedded.venues[0].state.name},{"\u00A0"}
+                            {event._embedded.venues[0].country.name}
+                            <br />
+                            {event._embedded.venues[0].postalCode}
+                            <br />
+                            <br />
+                            {event._embedded.venues[0].name} Seatmap:
+                          </Typography>
+                        </CardContent>
+                        <CardActions>
+                          <CardMedia
+                            component="img"
+                            className="seatmap"
+                            srcSet={event.seatmap.staticUrl}
+                            title={event._embedded.venues[0].name + " seatmap"}
+                            alt={event._embedded.venues[0].name + " seatmap"}
+                          />
+                        </CardActions>
+                        <CardMedia />
+                        {/* add time, doors, show */}
+                        {/* add price range */}
+                        {/* add capacity */}
+                        {/* add age restrictions */}
+                        {/* add purchase button */}
+                      </Card>
+                    </Box>
+                  </div>
+                );
+              } else {
+                return null;
+              }
+            })
+          );
+        });
+    };
+    loadData();
+  }, [params.id]);
 
   return (
-    <div className="event-container">
-      <h2 className="title"></h2>
-      <div className="venue"></div>      
-      <div className="price"></div>
-      <img src="" className="event-img"></img>
-      <img src="" className="seatmap"></img>
-    <ProductCard product={productId} />
+    <div>
+      <div>{events}</div>
     </div>
   );
-}
-
-function displayData(id) {
-  fetch(
-    "https://app.ticketmaster.com/discovery/v2/events/" +
-      id +
-      ".json?apikey=zhJDIqfMsloXKpRjywIbmnUSBGw9AxNq",
-    {}
-  )
-    .then((data) => data.json())
-    .then((json) => {
-      console.log(JSON.stringify(json));
-      document.querySelector(".title").innerHTML = json.name;
-      document.querySelector(".venue").innerHTML = json.name;
-      document.querySelector(".seatmap").src = json.seatmap.staticUrl;
-      document.querySelector(".event-img").src = json.images[0].url;
-      document.querySelector(".price").innerHTML =
-        "Ticket price: Min: " +
-        json.priceRanges[0].min +
-        "------ Max: " +
-        json.priceRanges[0].max;
-    });
 }
