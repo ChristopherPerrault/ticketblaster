0972bb4d6283e8f12eeeb616779cae06408bc37a
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2022-12-07 07:37:46 -0500

    account page styling

--- frontend/src/productsStore.js
+++ frontend/src/productsStore.js
@@ -5,19 +5,9 @@
 const productsArray = [
   {
     id: "price_1LnUTFDM1jwCEz8OGoOSXiSM",
-    title: "Coffee",
-    price: 4.99,
-  },
-  {
-    id: "price_1LnUTxDM1jwCEz8OAqHYTwKQ",
-    title: "Sunglasses",
-    price: 9.99,
-  },
-  {
-    id: "price_1LnUUoDM1jwCEz8OvxIcJ7to",
-    title: "Camera",
-    price: 39.99,
-  },
+    title: "TicketBlaster tickets",
+    price: 80.00,
+  }
 ];
 
 function getProductData(id) {
