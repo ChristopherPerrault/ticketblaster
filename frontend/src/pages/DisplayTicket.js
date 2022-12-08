610e7e86e795047e298f86c7db4ccae71171bc21
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2022-12-08 13:41:42 -0500

    hardcoded qr code to display

--- frontend/src/pages/DisplayTicket.js
+++ frontend/src/pages/DisplayTicket.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+import "../index.css";
+import Ticket from "../components/Ticket";
+import QR from "../assets/images/qrCode.png";
 import {
   Card,
   Box,
@@ -16,21 +18,19 @@ export default function DisplayTicket(props) {
     navigate(-1);
   };
   return (
-    <div>
-     
-      <Box>
-        <Typography variant="h2">
-            Enjoy the show!
-        </Typography>
-        <Card>
-            <CardMedia>
-
-            </CardMedia>
-        </Card>
-        <Button variant="contained" onClick={handleClick} >
-            Go back
-        </Button>
-      </Box>
-    </div>
+    <Box>
+      <h1>hello</h1>
+      <Typography variant="h2">Enjoy the show!</Typography>
+      <Typography variant="h3">{props.totalTickets}</Typography>
+      <img
+        src={QR}
+        alt="Use this to get in at the venue!"
+        className="qr-img"
+        style={{width:"300px",height:"300px"}}
+      />
+      <Button variant="contained" onClick={handleClick}>
+        Go back
+      </Button>
+    </Box>
   );
 }
