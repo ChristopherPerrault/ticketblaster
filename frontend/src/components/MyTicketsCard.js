d1bd897849acd76aea25a422d447526fc8645d18
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2022-12-08 13:49:56 -0500

    changed route, atm it just statically generates qr code

--- frontend/src/components/MyTicketsCard.js
+++ frontend/src/components/MyTicketsCard.js
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 export default function MyTicketCard(props) {
    const navigate = useNavigate();
    const handleClick = () => {
-     navigate(`/ticket/${props.id}`);
+     navigate(`/ticket`);
    };
   return (
     <div>
